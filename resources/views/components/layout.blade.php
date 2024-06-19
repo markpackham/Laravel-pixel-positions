3b2a6e10e5b52e23fa27ce20f3971da61f8bef76
mark <markpackham1@gmail.com>
2024-06-19 15:11:26 +0100

    Add tailwind
    https://youtu.be/26-cqMlTu4E?si=aZvYRzzaZQs_Ty-d&t=486

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,15 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
     <div>
-        <nav>
+        <nav class="flex justify-between">
             <div>
                 <a href="{{Vite::asset('resources/images/logo.svg')}}"><img src="" alt="logo"></a>
                 </div>
-            <div>links</div>
-            <div>post a job</div>
+            <div>
+                <a href="">Jobs</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
+            </div>
+            <div><a href="">Post a Job</a></div>
         </nav>
 
         <main>{{$slot}}</main>
