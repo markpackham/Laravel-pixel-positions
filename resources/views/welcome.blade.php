0259be27e072637b5fbd599b4cc51d2126312085
mark <markpackham1@gmail.com>
2024-06-19 15:56:42 +0100

    Style job listing layout
    https://youtu.be/26-cqMlTu4E?si=_RDH7d6DY1veiDVr&t=800

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,21 @@
 <x-layout>
-    Hello World
+    <section>
+        <h3>Top Jobs</h3>
+        <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+            <div class="self-start text-sm">Laracasts</div>
+            <div>
+                <h3>Video Producer</h3>
+                <p>Full Time - $60,000</p>
+            </div>
+            <div>
+                <div>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                </div>
+
+                <img src="http://placehold.it/42/42" alt="">
+            </div>
+        </div>
+    </section>
 </x-layout>
