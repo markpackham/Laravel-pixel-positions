7c083c31d92a1276e49d22ba1a5ebdf972412d4e
mark <markpackham1@gmail.com>
2024-06-19 17:03:08 +0100

    Add wide version for job cards
    https://youtu.be/iCn0NxC8osY?si=XhkXZWzNBrmPwO9w&t=45

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,10 +13,24 @@
 
         <section>
             <x-section-heading>Tags</x-section-heading>
+
+            <div class="mt-6 space-x-1">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
         </section>
 
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
+
         </section>
     </div>
 </x-layout>
