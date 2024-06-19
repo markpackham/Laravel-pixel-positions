b5dd531c12d893f347f8609119d720335fa19831
mark <markpackham1@gmail.com>
2024-06-19 16:04:29 +0100

    Add smooth transition when hovering over tag colors
    https://youtu.be/26-cqMlTu4E?si=KXnRPHkOdgCkv8QK&t=885

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,16 @@
 <x-layout>
     <section>
-        <h3>Top Jobs</h3>
+        <h3 class="mb-6">Top Jobs</h3>
         <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
             <div class="self-start text-sm">Laracasts</div>
-            <div>
+            <div class="py-8 font-bold">
                 <h3>Video Producer</h3>
                 <p>Full Time - $60,000</p>
             </div>
-            <div>
+
+            <div class="flex justify-between items-center mt-auto">
                 <div>
-                    <a href="#">Tag</a>
-                    <a href="#">Tag</a>
-                    <a href="#">Tag</a>
+                    <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
                 </div>
 
                 <img src="http://placehold.it/42/42" alt="">
