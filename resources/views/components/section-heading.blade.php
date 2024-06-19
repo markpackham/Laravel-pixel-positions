799d076630ab41e23c9e0b6bbbdbd569e9a3a3da
mark <markpackham1@gmail.com>
2024-06-19 16:45:41 +0100

    Add space to sections
    https://youtu.be/26-cqMlTu4E?si=ykKguBx-rFwb9roj&t=1100

--- resources/views/components/section-heading.blade.php
+++ resources/views/components/section-heading.blade.php
@@ -1,1 +1,8 @@
-<h3>{{$slot}}</h3>
+<div class="inline-flex items-center gap-x-2">
+    <span class="w-2 h-2 bg-white inline-block"></span>
+
+    <h3 class="font-bold text-lg">
+        {{$slot}}
+    </h3>
+
+</div>
