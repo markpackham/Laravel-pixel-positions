91ed49c02084b1fe62659a9ec83dafc2b8080a37
mark <markpackham1@gmail.com>
2024-06-20 20:17:24 +0100

    Add classes variable in panel blade template
    https://youtu.be/iCn0NxC8osY?feature=shared&t=1037

--- resources/views/components/panel.blade.php
+++ resources/views/components/panel.blade.php
@@ -1,2 +1,6 @@
+@php
+    $classes = 'p-4 bg-white/5 rounded-xl border border-transparent hover:border-blue-600 group transition-colors duration-300';
+@endphp
+
 {{-- Using shorthand for $attributes->merge by calling it as a function --}}
-<div {{$attributes(['class'=> 'p-4 bg-white/5 rounded-xl border border-transparent hover:border-blue-600 group transition-colors duration-300'])}}>{{$slot}}</div>
+<div {{$attributes(['class'=> $classes])}}>{{$slot}}</div>
