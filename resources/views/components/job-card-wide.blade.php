96b513e3fa1dafd8b9528635bafd28357d61f5f9
mark <markpackham1@gmail.com>
2024-06-20 19:44:00 +0100

    Add color transitions for card hovers
    https://youtu.be/iCn0NxC8osY?feature=shared&t=853

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-600 group transition-colors duration-300">
     <div>
         <x-employer-logo />
     </div>
@@ -7,7 +7,7 @@
 
         <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
 
-        <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
 
         <p class="text-sm text-gray-400 mt-auto">Full Time - $60,000</p>
     </div>
