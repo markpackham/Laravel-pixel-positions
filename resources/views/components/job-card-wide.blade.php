f9d9c5d8a47b9ac2f2700bfa7fa8a04cbe35b389
mark <markpackham1@gmail.com>
2024-06-20 20:00:31 +0100

    Add blade panel
    https://youtu.be/iCn0NxC8osY?feature=shared&t=880

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,21 +1,24 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-600 group transition-colors duration-300">
-    <div>
-        <x-employer-logo />
+    <div
+        class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-600 group transition-colors duration-300">
+
+        <div>
+            <x-employer-logo />
+        </div>
+
+        <div class="flex-1 flex flex-col">
+
+            <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+
+            <h3
+                class="font-bold text-xl mt-3 group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">
+                Video Producer</h3>
+
+            <p class="text-sm text-gray-400 mt-auto">Full Time - $60,000</p>
+        </div>
+
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
     </div>
-
-    <div class="flex-1 flex flex-col">
-
-        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
-
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
-
-        <p class="text-sm text-gray-400 mt-auto">Full Time - $60,000</p>
-    </div>
-
-    <div>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-    </div>
-
-</div>
