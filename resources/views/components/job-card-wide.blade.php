12aa739fe1d0d93dc4df5092a70fecb2a7059ee8
mark <markpackham1@gmail.com>
2024-06-20 18:49:12 +0100

    Get random number of picsum
    https://youtu.be/iCn0NxC8osY?feature=shared&t=275

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,9 +1,10 @@
 <div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
     <div>
-        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+        {{-- Get random images off picusm with random seed --}}
+        <img src="http://picsum.photos/seed/{{rand(0,10000)}}/100/100" alt="" class="rounded-xl">
     </div>
 
-    <div>
+    <div class="flex-1">
 
         <a href="#" class="self-start text-sm">Laracasts</a>
 
