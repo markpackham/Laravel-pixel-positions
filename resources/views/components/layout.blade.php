9c25ff9f62464fbdd952aa99abb064e8969c0823
mark <markpackham1@gmail.com>
2024-06-20 19:37:36 +0100

    Make job card title text blue on hover using tailwind "group"
    https://youtu.be/iCn0NxC8osY?feature=shared&t=716

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
     <title>Pixel Positions</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@400;500;600&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body class="bg-black text-white font-hanken-grotesk">
