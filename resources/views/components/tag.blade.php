2992b708900046f8a16b8ba63168b2a2ab59c776
mark <markpackham1@gmail.com>
2024-06-20 20:25:58 +0100

    Allow for small & large tags
    https://youtu.be/iCn0NxC8osY?feature=shared&t=1170

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,16 @@
-<a href="#" class="bg-white/10 hover:bg-white/25 px-3 py-1 rounded-xl text-2xs font-bold transition-colors duration-300">{{$slot}}</a>
+@props(['size'=>'base'])
+
+@php
+$classes = 'bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300';
+
+    if($size === 'base'){
+        // Leave a space at start so they aren't merged when concatinating to the rest of the class string
+        $classes .= " px-5 py-1 text-sm";
+    }
+
+    if($size === 'small'){
+        $classes .= " px-3 py-1 text-2xs";
+    }
+@endphp
+
+<a href="#" class="{{$classes}}">{{$slot}}</a>
