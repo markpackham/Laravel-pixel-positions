32ca4fd395baf809ef8fe04fc71793ccdb03e6fb
mark <markpackham1@gmail.com>
2024-06-24 09:45:06 +0100

    Arrange, Act & Assert test for job & employer
    https://youtu.be/NGB1sIvM4Ec?si=N49mfpQhQzQ5XaG0&t=977

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\Models\Employer;
+use App\Models\Job;
+
 // In this context, makes more sense to use "it" rather than "test"
 it('belongs to an employer', function () {
-    expect(true)->toBeTrue();
+    // Arrange (setup the World), Act, Assert - AAA
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    // Act & Assert
+    expect($job->employer->is($employer))->toBeTrue();
 });
