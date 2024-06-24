e3a08ca560db523e39ba3701402e283393f698cc
mark <markpackham1@gmail.com>
2024-06-24 11:03:23 +0100

    Add route for Job homepage
    https://youtu.be/NGB1sIvM4Ec?si=9JezuZwbXITJL5ok&t=1517

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -13,7 +13,10 @@ class JobController extends Controller
      */
     public function index()
     {
-        //
+        return view('jobs.index', [
+            'jobs' => '',
+            'tags' => ''
+        ]);
     }
 
     /**
