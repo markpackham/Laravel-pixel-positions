f2de48067e46288ea67c087c4c7fd1e236de3124
mark <markpackham1@gmail.com>
2024-06-24 09:52:10 +0100

    Add test for job having a tag
    https://youtu.be/NGB1sIvM4Ec?si=HmPoSVLyuHJVYf0T&t=1151

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -14,3 +14,12 @@ it('belongs to an employer', function () {
     // Act & Assert
     expect($job->employer->is($employer))->toBeTrue();
 });
+
+// A post can have tags
+it('can have tags', function () {
+    $job = Job::factory()->create();
+
+    $job->tag('Frontend');
+
+    expect($job->tags)->toHaveCount(1);
+});
