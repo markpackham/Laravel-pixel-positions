4cc36e9778ca5f7930646ca6e7e930ea9f227d3d
mark <markpackham1@gmail.com>
2024-06-24 09:03:52 +0100

    Replace "test" with "it"
    https://youtu.be/NGB1sIvM4Ec?si=qRnaaYmnw1E6zuDY&t=807

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,5 +1,6 @@
 <?php
 
-test('example', function () {
+// In this context, makes more sense to use "it" rather than "test"
+it('belongs to an employer', function () {
     expect(true)->toBeTrue();
 });
