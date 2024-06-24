8f4fc56033bcb4c6fb43b5a18d8945c493d51a05
mark <markpackham1@gmail.com>
2024-06-24 10:26:28 +0100

    Remove guards for ALL models
    https://youtu.be/NGB1sIvM4Ec?si=vXUbaK9bEx7ZxMV4&t=1468

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Stop having to go in each model & setting various attributes from
+        // protected to unprotected so we can change stuff more easily
+        // when using forms
+        Model::unguard();
     }
 }
