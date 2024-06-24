09efa6c3e641519dcac92f4ab51b033d8363ac4b
mark <markpackham1@gmail.com>
2024-06-24 11:37:14 +0100

    Make job output dynamic
    https://youtu.be/NGB1sIvM4Ec?si=OjY88M5WtE88M6y0&t=1852

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -7,18 +7,18 @@
 
         <div class="flex-1 flex flex-col">
 
-            <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+            <a href="#" class="self-start text-sm text-gray-400">{{$job->employer->name}}</a>
 
             <h3
                 class="font-bold text-xl mt-3 group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">
-                Video Producer</h3>
+                {{$job->title}}</h3>
 
-            <p class="text-sm text-gray-400 mt-auto">Full Time - $60,000</p>
+            <p class="text-sm text-gray-400 mt-auto">{{$job->salary}}</p>
         </div>
 
         <div>
             @foreach ($job->tags as $tag)
-            <x-tag :$tag>Backend</x-tag>
+            <x-tag :$tag />
             @endforeach
         </div>
     </x-panel>
