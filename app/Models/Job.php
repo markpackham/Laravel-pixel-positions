274a08bbc33f87c0fe8a74a2be850a550130cd7f
mark <markpackham1@gmail.com>
2024-06-24 10:23:31 +0100

    Update job tag method connecting tag to job
    https://youtu.be/NGB1sIvM4Ec?si=DgbRfxtaRedGld9U&t=1421

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,17 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
     use HasFactory;
 
-    public function tag()
+    public function tag(string $name): void
     {
+        // Give me a tag from the table & if it doesn't exist create one
+        $tag = Tag::firstOrCreate(['name' => $name]);
 
+        // Update the pivot table between Job & Tag
+        $this->tags()->attach($tag);
     }
 
-    public function tags()
+    public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class);
     }
