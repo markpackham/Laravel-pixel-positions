7adda34ad0381c7bce6ffaf04754f714e078bf8b
mark <markpackham1@gmail.com>
2024-06-24 11:25:15 +0100

    Output jobs on homepage
    https://youtu.be/NGB1sIvM4Ec?si=NurkIVb4uJJqn7Bs&t=1681

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -13,4 +13,4 @@ $classes = 'bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors
     }
 @endphp
 
-<a href="#" class="{{$classes}}">{{$tag->name}}</a>
+<a href="/tags/{{strtolower($tag->name)}}" class="{{$classes}}">{{$tag->name}}</a>
