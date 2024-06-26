fd95871a2ac0cebd126904699c440b369cfb10c0
mark <markpackham1@gmail.com>
2024-06-26 17:51:35 +0100

    Tidy up layout view

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,28 +1,32 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@400;500;600&display=swap" rel="stylesheet">
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black text-white font-hanken-grotesk">
+
+<body class="bg-black text-white font-hanken-grotesk pb-20">
     <div class="px-10">
-        {{-- Border white with an opacity of 10% --}}
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
                 <a href="/">
-                    <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="logo"></a>
-                </div>
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
+
             <div class="space-x-6 font-bold">
-                <a href="">Jobs</a>
-                <a href="">Careers</a>
-                <a href="">Salaries</a>
-                <a href="">Companies</a>
+                <a href="/">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
             </div>
 
             {{-- @auth --}}
@@ -36,17 +40,21 @@
                     <button>Log Out</button>
                 </form>
             </div>
-        {{-- @endauth --}}
+            {{-- @endauth --}}
 
-        @guest
+            @guest
             <div class="space-x-6 font-bold">
-                <a href="/register">Register</a>
+                <a href="/register">Sign Up</a>
                 <a href="/login">Log In</a>
             </div>
-        @endguest
+            @endguest
         </nav>
 
-        <main class="mt-10 max-w-[986px] mx-auto">{{$slot}}</main>
+        <main class="mt-10 max-w-[986px] mx-auto">
+            {{ $slot }}
+        </main>
     </div>
+
 </body>
+
 </html>
