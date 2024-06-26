3f2a785a24b1f83d2a93c2adadd4c39c93e66d3f
mark <markpackham1@gmail.com>
2024-06-26 19:07:11 +0100

    Add results view for searches
    https://youtu.be/B0zimPfuznw?si=tQXjZa57yQ5Yh84O&t=1367

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -9,6 +10,8 @@ class SearchController extends Controller
     // Use __invoke() when a controller only needs to have 1 action
     public function __invoke()
     {
-        dd("Hello");
+        $jobs = Job::where('title', 'LIKE', '%' . request('q') . '%')->get();
+
+        return view('results', ['jobs' => $jobs]);
     }
 }
