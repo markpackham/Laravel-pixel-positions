80c5f377c3fe2b3fe92a86cbc2962a721b60fc51
mark <markpackham1@gmail.com>
2024-06-26 18:39:44 +0100

    Add search bar to homepage
    https://youtu.be/B0zimPfuznw?si=XOKdEo8iG8-VnJlP&t=1155

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,9 +4,15 @@
         <section class="text-center pt-6">
             <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
 
-            <form action="" class="mt-6">
+            {{-- <form action="" class="mt-6">
                 <input type="text" placeholder="Web Dev" class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
-            </form>
+            </form> --}}
+
+            <x-forms.form action="/search" class="mt-6">
+                {{-- Name "q" for query is a common practice for search boxes --}}
+                {{-- We don't need a label so we set it to false or our field.blade.php component would break--}}
+            <x-forms.input :label="false" name="q" placeholder="Web Dev..." />
+            </x-forms.form>
         </section>
 
         <section class="pt-10">
