974e1cf10737a3edc93bfb9c96966b263d0099a8
mark <markpackham1@gmail.com>
2024-06-27 13:54:48 +0100

    Remove style duplication

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -10,7 +10,7 @@
             <a href="#" class="self-start text-sm text-gray-400">{{$job->employer->name}}</a>
 
             <h3
-                class="font-bold text-xl mt-3 group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">
+                class="font-bold mt-3 group-hover:text-blue-800 text-xl transition-colors duration-300">
                 <a href="{{$job->url}}" target="_blank">{{$job->title}}</a></h3>
 
             <p class="text-sm text-gray-400 mt-auto">{{$job->salary}}</p>
