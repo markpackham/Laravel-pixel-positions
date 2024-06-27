17b36d874f5741df7d7681708cb8a348ff9d7bf7
mark <markpackham1@gmail.com>
2024-06-27 13:58:59 +0100

    Leave not to turn shields of if using Brave browser

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,6 +4,8 @@
         <section class="text-center pt-6">
             <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
 
+            <p class="italic mt-2">When using Brave Browser turn Shields off or images won't show.</p>
+
             {{-- <form action="" class="mt-6">
                 <input type="text" placeholder="Web Dev" class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl">
             </form> --}}
