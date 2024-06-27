bc0ab1d9ab37a68389aaa984223fcc5ac61e7d39
mark <markpackham1@gmail.com>
2024-06-27 10:59:20 +0100

    Use Employer logo
    https://youtu.be/B0zimPfuznw?si=jA0t1USuBk_eLVSd&t=2099

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,4 +1,4 @@
 {{-- Get random images off picusm with random seed --}}
 {{-- We only need to provide a width to get square shaped images since same value used in height --}}
-@props(['width'=> 90])
-<img src="http://picsum.photos/seed/{{rand(0,10000)}}/{{$width}}" alt="" class="rounded-xl">
+@props(['employer','width'=> 90])
+<img src="{{$employer->logo}}" alt="{{$employer->name}}" class="rounded-xl">
