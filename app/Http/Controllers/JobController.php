582294012ba893df1a4d4dda162d5645511cee39
mark <markpackham1@gmail.com>
2024-06-27 10:44:36 +0100

    Fix order of jobs shown
    https://youtu.be/B0zimPfuznw?si=1ajKdHww2JchnWFW&t=2008

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -16,12 +16,12 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::all()->groupBy('featured');
+        $jobs = Job::latest()->with(['employer', 'tags'])->get()->groupBy('featured');
 
         return view('jobs.index', [
-            'featuredJobs' => $jobs["true"],
-            'jobs' => $jobs["false"],
-            'tags' => Tag::all()
+            'jobs' => $jobs[0],
+            'featuredJobs' => $jobs[1],
+            'tags' => Tag::all(),
         ]);
     }
 
